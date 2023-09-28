bac8994aec234b8095b4f0dc9c064f608fde0211
hpetrovych <gordy.cp96@gmail.com>
2023-09-28 12:35:42 +0300

    first commit

---
+++ token/token.go
@@ -0,0 +1,64 @@
+package token
+
+type Type string
+
+const (
+	ILLEGAL Type = "ILLEGAL"
+	EOF          = "EOF"
+
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT = "<"
+	GT = ">"
+
+	EQ  = "=="
+	NEQ = "!="
+	GTE  = ">="
+	LTE = "<="
+
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
+)
+
+type Token struct {
+	Type    Type
+	Literal string
+}
+
+var keywords = map[string]Type{
+	"fn": FUNCTION,
+	"let": LET,
+	"true": TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+}
+
+func LookupIdent(ident string) Type {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
